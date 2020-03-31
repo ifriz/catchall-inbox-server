f5e9c909046249a716f9afde4946e2a677174fec
Ian Frizzell <ian.frizzell@gmail.com>
2020-03-31 08:30:31 -0600

    added logging, added get to email_handler.py to enable fetching an email by id.

--- catchallinbox.py
+++ catchallinbox.py
@@ -1,12 +1,25 @@
 ###
 # catch all inbox
 ###
-
+import logging
 from pycnic.core import WSGI
-from email_handler import EmailHandler 
+from email_handler import EmailHandler
+
 
 class app(WSGI):
+    logger = logging.Logger("catchall_inbox_servera")
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    ch.setFormatter(formatter)
+
+    logger.addHandler(ch)
+
+    logger.info("setting up routing")
+
     routes = [
         ('/', EmailHandler()),
-        (r'/email/([\w]+)', EmailHandler())
+        (r'/email', EmailHandler())
     ]
--- email_handler.py
+++ email_handler.py
@@ -1,10 +1,11 @@
+import json
+import logging
 from pycnic.core import Handler
 from pycnic.errors import HTTP_400
-
 from email_service import email_service
 
-
 class EmailHandler(Handler):
+    logger = logging.getLogger("catchall_inbox_servera")
 
     def post(self):
         """ get an emails for the specified email address """
@@ -12,14 +13,16 @@ class EmailHandler(Handler):
         if not self.request.data.get('email'):
             raise HTTP_400("provide an email address")
 
+
         email_address = self.request.data.get('email')
 
         emails = []
         # emails
 
+        self.logger.info(f"fetching emails for {email_address}")
         service = email_service.EmailService()
 
-        emails = service.find_emails(email_address)
+        emails = service.get_emails(email_address)
 
         data = []
 
@@ -32,3 +35,23 @@ class EmailHandler(Handler):
             data.append(message_data)
 
         return data
+
+    def get(self):
+        if not self.request.args.get('mid'):
+            raise HTTP_400("no message id listed")
+
+        message_id = self.request.args.get('mid')
+
+        self.logger.info(f'fetching message {message_id}')
+
+        service = email_service.EmailService()
+        email_message = service.get_email_by_id(message_id)
+
+        return (
+            {
+                # "headers": email_message['headers'],
+                # "to": email_message['to'],
+                "from": email_message['from'],
+                "body": json.dumps(email_message['body'].decode('utf-8'))
+            }
+        )
