d1954c25d67c38656892b4e85827ccf55b0b3b43
Ian Frizzell <ian.frizzell@gmail.com>
2020-04-15 10:18:53 -0600

    refactored email service tests to account for success message.

--- tests/email_service_test.py
+++ tests/email_service_test.py
@@ -1,11 +1,8 @@
-from email.message import Message
-
-import pytest
 from email_service import email_service
 
 
 def test_pass():
-    assert True == True
+    assert True is True
 
 
 def test_get_email_load_credentials_from_file():
@@ -13,38 +10,41 @@ def test_get_email_load_credentials_from_file():
     assert service.hostname is not None
 
 
-# To test a successful request to get an email, uncomment this test
-# Provide an email address that is known to exist in the mailbox.
-# def test_get_emails():
-#     service = email_service.EmailService()
-#     emails = service.get_emails('existingemail@example.com')
-#
-#     assert len(emails) > 0
+def test_get_email_invalid_address():
+    service = email_service.EmailService()
+    response = service.get_emails('@example.com')
+    assert response["success"] is False
 
 
 def test_get_email_not_provided():
     service = email_service.EmailService()
-    emails = service.get_emails(None)
+    response = service.get_emails(None)
+    assert response["success"] is False
 
-    assert emails is None
+
+# To test a successful request to get an email, uncomment this test
+# Provide an email address that is known to exist in the mailbox.
+# def test_get_emails_is_successful():
+#     service = email_service.EmailService()
+#     response = service.get_emails('test@example.com')
+#     assert response["success"] is True
 
 
-def test_get_email_invalid_email():
-    service = email_service.EmailService()
-    emails = service.get_emails("abcd")
-
-    assert emails is None
+# To test a successful request to get an email, uncomment this test
+# Provide an email address that is known to exist in the mailbox.
+# def test_get_emails_has_values():
+#     service = email_service.EmailService()
+#     response = service.get_emails('test@example.com')
+#     assert len(response["emails"]) > 0
 
 
 def test_get_email_by_invalid_id():
     s = email_service.EmailService()
     m = s.get_email_by_id(b'1')
-
     assert m is None
 
 # To test for a successful call to get an email by ID, uncomment this test and provide a known email message id
 # def test_get_email_by_id():
 #     service = email_service.EmailService()
-#     email_message = service.get_email_by_id(b'3215')
-#
+#     email_message = service.get_email_by_id(b'0000')
 #     assert email_message.get('subject') is not None
